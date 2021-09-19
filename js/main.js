2571a559ea9ccb506b879893d9003f450e8f8d47
Skalyan <itlutsk@gmail.com>
2021-09-19 23:47:34 +0300

    Promo section 2021-09-19

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(function(){
-
+    $('.promo__slider').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="promo__slider_btn btn__prev"><img src="images/btn_slider_left.svg" /></button>',
+        nextArrow: '<button type="button" class="promo__slider_btn btn__next"><img src="images/btn_slider_right.svg" /></button>'
+    });
 });
